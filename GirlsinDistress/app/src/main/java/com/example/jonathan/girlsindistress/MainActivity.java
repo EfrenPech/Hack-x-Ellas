73fa15ffa7dd5db52289b94ff1c4ed06316fae72
Sergio Efren Pech Pool <Sergio Efren Pech Pool>
2016-11-18 16:01:55 -0500

    Interface Menu Final

--- GirlsinDistress/app/src/main/java/com/example/jonathan/girlsindistress/MainActivity.java
+++ GirlsinDistress/app/src/main/java/com/example/jonathan/girlsindistress/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.jonathan.girlsindistress;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton mapa = (ImageButton)findViewById(R.id.btn1);
+        ImageButton denuncias =(ImageButton)findViewById(R.id.btn2);
+        ImageButton informacion = (ImageButton)findViewById(R.id.btn3);
+        ImageButton contacto = (ImageButton)findViewById(R.id.btn4);
+        contacto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent VentanaContacto = new Intent(MainActivity.this, com.example.jonathan.girlsindistress.VentanaContacto.class);
+                startActivity(VentanaContacto);
+            }
+        });
+        informacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent VentanaInfo= new Intent(MainActivity.this, com.example.jonathan.girlsindistress.VentanaInfo.class);
+                startActivity(VentanaInfo);
+            }
+        });
+        denuncias.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent VentanaDenuncias = new Intent(MainActivity.this, com.example.jonathan.girlsindistress.VentanaDenuncias.class);
+                startActivity(VentanaDenuncias);
+            }
+        });
+        mapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ventanaMapa = new Intent(MainActivity.this,VentanaMapa.class);
+                startActivity(ventanaMapa);
+            }
+        });
     }
 }
---
+++ GirlsinDistress/app/src/main/java/com/example/jonathan/girlsindistress/VentanaMapa.java
@@ -0,0 +1,13 @@
+package com.example.jonathan.girlsindistress;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VentanaMapa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana_mapa);
+    }
+}
