e018cafe6a607be37172ce7ea3dab34b928ab449
DESKTOP-I3AFH1C\Jonathan <130300114@ucaribe.edu.mx>
2016-11-18 14:40:03 -0500

    Mapa

--- GirlsinDistress/app/src/main/java/com/example/jonathan/girlsindistress/MainActivity.java
+++ GirlsinDistress/app/src/main/java/com/example/jonathan/girlsindistress/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.jonathan.girlsindistress;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button orderButton = (Button)findViewById(R.id.button2);
+        //--- BOTON QUE REDIRECCIONA A OTRA ACTIVIDAD--//
+        orderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //       Intent intent = new Intent(FirstActivity.this, OrderScreen.class);
+                Intent button_uno = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(button_uno);
+            }
+        });
+        //---END BOTON---//
+
+
     }
 }
