c0fbe017c52fa8149e513a2b2c42b8d3aa9b9ab1
DESKTOP-I3AFH1C\Jonathan <130300114@ucaribe.edu.mx>
2016-11-19 05:27:40 -0500

    Actualización denuncias pinche Efren y la Api?

--- GirlsinDistress/app/src/main/java/com/example/jonathan/girlsindistress/VentanaDenuncias.java
+++ GirlsinDistress/app/src/main/java/com/example/jonathan/girlsindistress/VentanaDenuncias.java
@@ -1,31 +1,105 @@
 package com.example.jonathan.girlsindistress;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class VentanaDenuncias extends MapsActivity {
-    TextView latitud = (TextView)findViewById(R.id.Latitud);
-    TextView longitud =(TextView)findViewById(R.id.Longitud);
+    TextView latitud ;
+    TextView longitud ;
     double latitude=0.0;double longitude=0.0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ventana_denuncias);
-       // miUbicacion();
-        latitud.setText("Latitud: ");
-        longitud.setText("Longitud: ");
+        latitud = (TextView)findViewById(R.id.Latitud);
+        longitud =(TextView)findViewById(R.id.Longitud);
+        Thread t = new Thread() {
 
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(1000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // update TextView here!
+                                latitud.setText("Latitud: "+latitude);
+                                longitud.setText("Longitud: "+longitude);
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+
+        t.start();
 
     }
 
+
+    public void actualizarUbicacion(Location location) {
+        if (location != null) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+           // agregarMarcador(latitude, longitude);
+        }
+    }
+
+    LocationListener locListener = new android.location.LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            actualizarUbicacion(location);
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    private void miUbicacion() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            } else {
+                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                actualizarUbicacion(location);
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 15000, 0, locListener);
+            }
+        } else {
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            actualizarUbicacion(location);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 15000, 0, locListener);
+        }
+    }
 
 }
